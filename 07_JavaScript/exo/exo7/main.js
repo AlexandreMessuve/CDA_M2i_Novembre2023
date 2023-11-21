3a801ad1b82de01be1af4a9bf7092ac0767f6033
Messuve Alexandre <alexandre.messuves@gmail.com>
2023-11-21 10:00:16 +0100

    ajout de l'exo7

---
+++ 07_JavaScript/exo/exo7/main.js
@@ -0,0 +1,21 @@
+let nombre = 1;
+
+//boucle for
+console.log("Début de la boucle for : \n");
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i + "\n");
+    
+}
+console.log("Fin de la boucle for : \n");
+
+//boucle while 
+
+console.log("Début de la boucle while : \n");
+
+while (nombre <= 10) {
+    console.log(nombre + "\n");
+    nombre++
+}
+
+console.log("Fin de la boucle while : \n");
