206b2debedd80e5edd5c7e93e5f83b0fe98df1e4
Messuve Alexandre <alexandre.messuves@gmail.com>
2023-11-21 09:24:44 +0100

    ajout des exos JS

--- 07_JavaScript/exo/exo5/main.js
+++ 07_JavaScript/exo/exo5/main.js
@@ -2,4 +2,4 @@ let temperature;
 
 temperature = Number(prompt('Veuillez entrer une température :'));
 
-alert(temperature < 0 ? "SOLIDE" : temperature <= 100 ? "LIQUIDE" : temperature > 100 ? "GAZEUX" : "ERREUR"  );
+alert((temperature < 0 ? "SOLIDE" : temperature <= 100 ? "LIQUIDE" : (temperature > 100 ? "GAZEUX" : "ERREUR" )));
---
+++ 07_JavaScript/exo/exo6/main.js
@@ -0,0 +1,30 @@
+let age, paid, yearExp, condition, reason;
+
+
+age = Number(prompt('Veuillez entrer votre age :'));
+
+paid = Number(prompt('Veuillez entrer votre salaire : '));
+
+yearExp = Number(prompt('Veuillez entrer vos années d\'expérience'));
+
+condition = age >= 30 && paid <= 40000 && yearExp >= 5;
+
+//version if 
+if(condition){
+    console.log("vous êtes éligible pour postuler.");
+}else{
+    if(age < 30){
+        reason = "Vous etes trop jeune. ";
+    }
+    if(paid > 40000){
+        reason += "Votre salaire est trop élévé. ";
+    }
+    if(yearExp < 5){
+        reason += "Vous n'avez pas l'expérience requise. ";
+    }
+    console.log("Vous n'êtes pas éligible. Raison: "+reason );
+}
+
+//version terniaire
+
+console.log(condition ? "Vous etes eligible" : "Vous n'êtes pas éligible raison: " + (age < 30 ? "Vous êtes trop jeune. " : "")+(paid > 40000 ? "Votre salaire est trop élevé. " : "")+(yearExp < 5 ? "Vous n'avez pas l'expérience requise. " : "") );
