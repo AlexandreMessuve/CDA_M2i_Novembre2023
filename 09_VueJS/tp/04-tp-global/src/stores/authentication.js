c56443dbe40ebbd9fa5306c2ac8a83e2a61cbe24
Alexandre MESSUVE <alexandre.messuves@gmail.com>
2023-12-15 16:57:52 +0100

    à finir chez moi

--- 09_VueJS/tp/04-tp-global/src/stores/authentication.js
+++ 09_VueJS/tp/04-tp-global/src/stores/authentication.js
@@ -1,12 +1,29 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+export const useAuthStore = defineStore('auth', () => {
+  const users = ref([]);
 
-  return { count, doubleCount, increment }
+  const user = ref(null);
+
+  const isLogging = computed(() => {
+    return !!user.value;
+  })
+
+  const auth = (email, password) => {
+    if(users.value > 0){
+      users.value.forEach((user) => {
+        if(email === user.email && password === user.password){
+          user.value = {
+            username: user.username,
+            email: user.email
+          }
+          return true;
+        }
+      });
+      return 'email ou mot de passe incorrect';
+    }else{
+      return 'vous n\'exister pas dans notre base de données';
+    }
+  }
 })
