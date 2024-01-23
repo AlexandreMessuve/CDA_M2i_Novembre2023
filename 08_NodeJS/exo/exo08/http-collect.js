c5fa2b58d5a07b72d20326d49ddbe8e07ddbf40f
Alexandre MESSUVE <alexandre.messuves@gmail.com>
2024-01-23 14:35:51 +0100

    maj exo08 learnyounode

--- 08_NodeJS/exo/exo08/http-collect.js
+++ 08_NodeJS/exo/exo08/http-collect.js
@@ -1,12 +1,15 @@
 const http = require('http');
-const bl = require('bl');
 http.get(process.argv[2], (response) => {
-    response.pipe(bl((err, data) => {
-        if(err){
-         return console.error(err);
-        }
-        console.log(data.toString().length)
-        console.log(data.toString())
-
-    }))
+    let collect = '';
+    response.setEncoding('utf-8');
+    response.on('error', (err) => {
+        if (err) throw err;
+    });
+    response.on('data',(data) => {
+        collect += data.toString();
+    });
+    response.on('end',() => {
+        console.log(collect.length);
+        console.log(collect);
+    })
 }).on('error', console.error);
