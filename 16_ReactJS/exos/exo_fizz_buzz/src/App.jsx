d4f765eca130bb92bbc9725685e1972caec17dd8
Alexandre MESSUVE <alexandre.messuves@gmail.com>
2024-02-19 15:56:08 +0100

    ajout exo fizzbuzz

---
+++ 16_ReactJS/exos/exo_fizz_buzz/src/App.jsx
@@ -0,0 +1,22 @@
+import {useState} from 'react'
+import './App.css'
+
+function App() {
+    const [count, setCount] = useState(1);
+    const incrementCount = () => {
+        setCount(count + 1);
+    }
+    const decrementCount = () => {
+        setCount(count - 1);
+    }
+
+    return (
+        <>
+            <p>{count % 3 === 0 && count % 5 === 0 ? 'FizzBuzz' : (count % 3 === 0 ? 'Fizz' : count % 5 === 0 ? 'Buzz' : count)}</p>
+            <button onClick={decrementCount} disabled={count <= 1}>-1</button>
+            <button onClick={incrementCount} disabled={count >= 100}>+1</button>
+        </>
+    )
+}
+
+export default App
