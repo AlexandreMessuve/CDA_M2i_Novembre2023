1664a88b9e6e829041ded67d44b859f80c285fb6
Alexandre MESSUVE <alexandre.messuves@gmail.com>
2024-02-19 15:59:43 +0100

    fix

--- 16_ReactJS/exos/exo_fizz_buzz/src/App.jsx
+++ 16_ReactJS/exos/exo_fizz_buzz/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 function App() {
     const [count, setCount] = useState(1);
+    const maxCount = 100;
     const incrementCount = () => {
         setCount(count + 1);
     }
@@ -14,7 +15,7 @@ function App() {
         <>
             <p>{count % 3 === 0 && count % 5 === 0 ? 'FizzBuzz' : (count % 3 === 0 ? 'Fizz' : count % 5 === 0 ? 'Buzz' : count)}</p>
             <button onClick={decrementCount} disabled={count <= 1}>-1</button>
-            <button onClick={incrementCount} disabled={count >= 100}>+1</button>
+            <button onClick={incrementCount} disabled={count >= maxCount}>+1</button>
         </>
     )
 }
