9bef0d40b7eccc5bf8a6eb9c1b137f60bd29c4f0
Alexandre MESSUVE <alexandre.messuves@gmail.com>
2024-03-13 09:57:43 +0100

    ajout exercice03 calculator react-native

--- 17_React_Native/exos/exercice03/App.jsx
+++ 17_React_Native/exos/exercice03/App.jsx
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import Calculator from './components/Calculator'
 import * as math from 'mathjs'
 
 const App = () => {
   const [result, setResult] = useState('0');
+  const [calcul, setCalcul] = useState('');
+  const [isLongResult, setIsLongResult] = useState(false);
   const [expression, setExpression] = useState('');
   
   const addToExpression = (value) => {
@@ -22,12 +24,14 @@ const App = () => {
   }
 
   const resetExpression = () => {
+    setCalcul('');
     setExpression('');
     setResult('0');
   }
   const calculate = () => {
     try{
       const res = math.evaluate(expression).toString();
+      setCalcul(expression + ' =');
       setResult(res);
       setExpression(res);
 
@@ -39,14 +43,20 @@ const App = () => {
     if(expression.length > 0){
       setResult(expression);
     }
-  },[expression])
+    if(result.length > 20){
+      setIsLongResult(true)
+    }else{
+      setIsLongResult(false)
+    }
+  },[expression,result])
   return (
     <View style={styles.view}>
       <View style={styles.viewTitle}>
         <Text style={styles.title}>Calculator</Text>
       </View>
       <View>
-        <Text style={styles.number}>{result}</Text>
+        <Text style={styles.calcul}>{calcul.length > 0 && calcul}</Text>
+        <Text style={[styles.number, isLongResult && styles.numberSize]}>{result}</Text>
       </View>
       <Calculator resetExpression={resetExpression} addToExpression={addToExpression} calculate={calculate} delExpression={delExpression}/>
     </View>
@@ -62,16 +72,26 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   title: {
-    marginTop: 100,
+    marginTop: 50,
     color: 'white',
     fontSize: 50,
     textAlign: 'center'
   },
+  calcul: {
+    fontSize: 30,
+    textAlign: 'right',
+    marginHorizontal: 20,
+    marginTop: 80,
+    color: 'antiquewhite'
+  },
   number:{
     marginHorizontal: 20,
-    marginVertical: 50,
+    marginVertical: 10,
     fontSize: 70,
     color: 'white',
     textAlign: 'right'
+  },
+  numberSize: {
+    fontSize: 50
   }
 })
