955d5bcd5f5fa8cdcac7c42c21cae684fed2e87e
Alexandre MESSUVE <alexandre.messuves@gmail.com>
2024-03-13 15:16:12 +0100

    maj formulaire

--- 17_React_Native/exos/exercice_bonus_contacts/components/FormContact.jsx
+++ 17_React_Native/exos/exercice_bonus_contacts/components/FormContact.jsx
@@ -1,39 +1,45 @@
 import { Button, Pressable, StyleSheet, Text, TextInput, View, Modal } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const FormContact = (props) => {
-    const [firstname, setFirstname] = useState('');
-    const [lastname, setLastname] = useState('');
-    const [phone, setPhone] = useState('');
-    const [address, setAddress] = useState('');
+    const [formData, setFormData] = useState({
+        firstname: '',
+        lastname: '',
+        phone: '',
+        address: ''
+    });
 
+    const handleChangeText = (index, value) => {
+        setFormData({...formData, [index]: value})
+    }
     const handleSubmit = () => {
-        const condition = firstname.length > 0 && lastname.length > 0 && phone.length > 0 && address.length > 0
+        const condition = formData.firstname.length > 0 && formData.lastname.length > 0 && formData.phone.length > 0 && formData.address.length > 0
         if (condition) {
-            const newContact = {
-                id: Date.now(),
-                firstname: firstname,
-                lastname: lastname,
-                phone: phone,
-                address: address
-            }
-            props.addContact(newContact);
-            setFirstname('');
-            setLastname('');
-            setPhone('');
-            setAddress('');
+            props.addContact(formData);
+            setFormData({
+                firstname: '',
+                lastname: '',
+                phone: '',
+                address: '',
+            })
         }else{
             alert('Tous les champs sont requis !')
         }
     }
+    useEffect(() => {
+        if(!formData.id){
+            setFormData({...formData, id: Date.now()});
+        }
+        console.log(formData);
+    }, [formData])
     return (
         <Modal visible={props.visible}>
             <View style={styles.view}>
                 <Text style={styles.title}>Ajouter un contact : </Text>
-                <TextInput style={styles.input} value={firstname} onChangeText={setFirstname} placeholder={'Prénom'} />
-                <TextInput style={styles.input} value={lastname} onChangeText={setLastname} placeholder={'Nom'} />
-                <TextInput style={styles.input} value={phone} onChangeText={setPhone} placeholder={'Téléphone'} />
-                <TextInput style={styles.input} value={address} onChangeText={setAddress} placeholder={'Adresse'} />
+                <TextInput style={styles.input} value={formData.firstname} onChangeText={(text) => handleChangeText('firstname' , text)} placeholder={'Prénom'} />
+                <TextInput style={styles.input} value={formData.lastname} onChangeText={(text) => handleChangeText('lastname' , text)} placeholder={'Nom'} />
+                <TextInput style={styles.input} value={formData.phone} onChangeText={(text) => handleChangeText('phone' , text)} placeholder={'Téléphone'} />
+                <TextInput style={styles.input} value={formData.address} onChangeText={(text) => handleChangeText('address' , text)} placeholder={'Adresse'} />
                 <View style={styles.viewButton}>
                     <Pressable
                         style={({ pressed }) => [styles.button, styles.buttonSubmit, (pressed) && styles.buttonSubmitPress]}
@@ -62,9 +68,12 @@ const styles = StyleSheet.create({
     input: {
         width: 400,
         height: 60,
+        borderWidth: 2,
         borderRadius: 40,
-        backgroundColor: 'antiquewhite',
-        margin: 10
+        borderColor: 'black',
+        backgroundColor: 'white',
+        margin: 10,
+        textAlign: 'center'
     },
     title: {
         fontSize: 40,
