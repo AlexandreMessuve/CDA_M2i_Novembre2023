ebbd27d85fdc740dfb2db67e99b47a1c4ad1afa8
Alexandre MESSUVE <alexandre.messuves@gmail.com>
2024-04-11 14:39:25 +0200

    maj exo pendu

--- 18_Java/02_POO/exos/exercice01/src/main/java/org/pendu/Main.java
+++ 18_Java/02_POO/exos/exercice01/src/main/java/org/pendu/Main.java
@@ -1,6 +1,7 @@
 package org.pendu;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
@@ -12,7 +13,7 @@ public class Main {
         String word = WordGenerator.generateWord();
         Pendu pendu = new Pendu(word);
         pendu = menu(pendu);
-
+        System.out.println("-".repeat(30));
         if (pendu.testWin()) {
             System.out.printf("Vous avez gagné ! le mot : %s", pendu.getWord());
         } else {
@@ -23,17 +24,39 @@ public class Main {
     private static Pendu menu(Pendu pendu) {
         System.out.print("Voulez vous definir le nombre d'essai ? (10 par defaut) ? Y/N ");
         char choice = scanner.next().toLowerCase().charAt(0);
+        int essai;
         if (choice == 'y') {
             System.out.print("Combien d'essai voulez vous ?");
-            int essai = scanner.nextInt();
+            while (true) {
+                try {
+                    essai = Integer.parseInt(scanner.next());
+                    if(essai < 0){
+                        throw new Exception("N'est pas positif");
+                    }
+                }catch (NumberFormatException e){
+                    System.out.println("Ce n'est pas un entier");
+                    continue;
+                }catch (Exception e){
+                    System.out.println("Saisie incorrect (négative)");
+                    continue;
+                }
+                break;
+            }
             pendu.setCount(essai);
         }
         do {
-            System.out.printf("%s (%s lettres) essai restant %s\n", pendu.getMaskedWord(), pendu.getMaskedWord().length(), pendu.getCount());
+
+            System.out.printf("Mot à trouvée : %s (%s lettres) \n Essai restant: %s\n", pendu.getMaskedWord(), pendu.getMaskedWord().length(), pendu.getCount());
             System.out.print("Entrez une lettre : ");
             String yourChar = scanner.nextLine();
-            pendu.testChar(yourChar.toLowerCase());
-        } while (pendu.getCount() != 0 || !pendu.testWin());
+            while (!Pattern.matches("[A-Za-z]", yourChar)){
+                System.out.print("Erreur ! veuillez reesayer : ");
+                yourChar = scanner.nextLine();
+            }
+            String resp = pendu.testChar(yourChar.toLowerCase());
+            System.out.println("-".repeat(30));
+            System.out.println(resp);
+        } while (pendu.getCount() != 0 && !pendu.testWin());
         return pendu;
     }
 
--- 18_Java/02_POO/exos/exercice01/src/main/java/org/pendu/Pendu.java
+++ 18_Java/02_POO/exos/exercice01/src/main/java/org/pendu/Pendu.java
@@ -3,11 +3,12 @@ package org.pendu;
 public class Pendu {
     private String maskedWord;
     private String word;
+    private String letterTested = "";
     private int count = 10;
 
     public Pendu(String word) {
         this.word = word.toLowerCase();
-        generateMaskedWord(word);
+        generateMaskedWord();
     }
     public String getMaskedWord() {
         return maskedWord;
@@ -29,24 +30,29 @@ public class Pendu {
         this.maskedWord = maskedWord;
     }
 
-    public void generateMaskedWord(String word) {
-        setMaskedWord("*".repeat(word.length()));
-    }
-    public void testChar (String ch) {
-        String newMaskedWord = "";
-        for (int i = 0; i < word.length(); i++) {
-            if(Character.toString(word.charAt(i)).equalsIgnoreCase(ch)) {
-                newMaskedWord += ch;
-                continue;
+    private void generateMaskedWord() {
+        String maskedWord = "";
+        for (char letter : word.toCharArray()) {
+            if(letterTested.contains(Character.toString(letter))) {
+                maskedWord += letter;
             }
-            if(getMaskedWord().charAt(i) == '*'){
-                newMaskedWord += "*";
-            }else{
-                newMaskedWord += getMaskedWord().charAt(i);
+            else{
+                maskedWord += "*";
             }
         }
-        setMaskedWord(newMaskedWord);
+        setMaskedWord(maskedWord);
+    }
+    public String testChar (String letter) {
+        if(letterTested.contains(letter)) {
+            return "Lettre déjà testé";
+        }
+        letterTested += letter;
         setCount(count - 1);
+        if(word.contains(letter)){
+            generateMaskedWord();
+            return "La lettre est dans le mot !";
+        }
+        return  "La lettre n'est pas dans le mot !";
     }
     public boolean testWin(){
         return getWord().equalsIgnoreCase(getMaskedWord());
